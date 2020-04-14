e5e64ffcd12caa1d9c38d32733dff5a9d495a97b
Aleksei-Zaichenko <aleksei.zaichenko@cix.csi.cuny.edu>
2020-04-13 21:28:22 -0400

    completed .pots .put .delete endpoints

--- accounts/accounts-router.js
+++ accounts/accounts-router.js
@@ -27,5 +27,50 @@ router.get('/:id', (req, res) => {
     })
 })
 
+router.post('/', (req, res) =>{
+    const accountData = req.body;
+    db('accounts').insert(accountData, 'id')
+    .then(newAccountID => {
+        res.status(200).json({message: `New account was created and added to database, ID:${newAccountID} `})
+    })
+    .catch(err =>{
+        console.log(err);
+        res.status(500).json({message: "error 500"});
+    })
+})
+
+router.put('/:id', (req, res) =>{
+    db('accounts')
+    .where({id: req.params.id})
+    .update(req.body)
+    .then(updated =>{
+        if(updated){
+            res.status(201).json({message: `account with ID:${req.params.id} was updated` })
+        } else {
+            res.status(500).json({message: `account with ID:${req.params.id} was  not updated` })
+        }
+    })
+    .catch(err =>{
+        console.log(err);
+        res.status(404).json({message: "error 404"});
+    })
+})
+
+router.delete('/:id', (req, res) =>{
+    db('accounts')
+    .where({id: req.params.id})
+    .del()
+    .then(deleted => {
+        if(deleted){
+            res.status(201).json({message: `account with ID:${req.params.id} was deleted` })
+        } else {
+            res.status(500).json({message: `account with ID:${req.params.id} was  not deleted` })
+        }
+    })
+    .catch(err =>{
+        console.log(err);
+        res.status(404).json({message: "error 404"});
+    })
+})
 
 module.exports = router;
